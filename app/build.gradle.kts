0f27cd583e0b804e0ca0a3d88965ef3eedccfe5e
DB <Danielboegh93@gmail.com>
2024-03-25 16:40:41 +0100

    Added tests for GameViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
 
+    testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
